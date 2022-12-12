866677b417e5cf02fdbf10d735d65c5ea15feef8
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2022-12-12 16:34:03 -0300

    Requisito 02 - feito

--- script.js
+++ script.js
@@ -8,6 +8,29 @@ const createHeader = () => {
   getApp.appendChild(genHeader);
 };
 
+const generatePallete = (div) => {
+  for (let i = 0; i < 4; i += 1) {
+    const createDiv = document.createElement('div');
+    createDiv.className = 'color';
+    createDiv.style.border = '1px solid black';
+    createDiv.style.width = '50px';
+    createDiv.style.height = '50px';
+
+    if (i % 2 === 1) {
+      createDiv.style.borderRadius = '50%';
+    }
+    div.appendChild(createDiv);
+  }
+};
+
+const createPallete = () => {
+  const createDiv = document.createElement('div');
+  createDiv.id = 'pallete';
+  generatePallete(createDiv);
+  getApp.appendChild(createDiv);
+};
+
 window.onload = () => {
   createHeader();
+  createPallete();
 };
