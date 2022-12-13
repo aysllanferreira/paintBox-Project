8eb86a16a9cf3a3dff5a834fce6433c4fa1952eb
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2022-12-13 18:44:14 -0300

    fix console log

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,13 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        message: 'Unexpected property on console object was called',
+      },
+    ],
   },
 };
